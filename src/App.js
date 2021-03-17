ec42535bbf8c9d54dc1b15c9e542eeabe709794c
apinan <iamapinan@gmail.com>
2021-03-17 12:27:43 +0700

    update logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import logo from './assets/images/getter-colored-sm.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="Getters" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome,  Website under constructioning. Please <a href="mailto:apinan@getters.app">Email to us</a> if you have any question.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
